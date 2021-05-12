9ad9623b02e0b2747ae3b89c90da6fd25bbf8000
Alejahndro <alejahndro02@gmail.com>
2021-05-12 13:43:24 -0500

    Se esta haceiendo la onsulta hacia la api de newsapi

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
+  headUrl = 'https://newsapi.org/v2/top-headlines?country='
+  apiKeyNewsApi = 'c469042076824bc2a9676aaca02b4f10';
+  constructor(private http: HttpClient) { 
+  }
 
-  constructor() { }
+  getNoticias(parametros: any):Observable<any>{
+    const URL = this.headUrl+parametros.pais+'&category='+parametros.categoria +
+                '&apiKey='+this.apiKeyNewsApi;
+    return this.http.get(URL);
+  }
 }
