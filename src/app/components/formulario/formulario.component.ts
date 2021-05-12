f0f28045b7515f6565e519be1b7da841d8269ccb
Alejahndro <alejahndro02@gmail.com>
2021-05-12 12:22:04 -0500

    Se declararon las variables categoriaSeleccionada, paisSeleccionado y variables de tipo arreglo como categorias y paises, tambien se creo el metodo buscarNoticia()

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  categoriaSeleccionada = "general";
+  paiseSeleccionado = "mx";
+  categorias : any[]= [
+    {value: 'general', nombre:'General'},
+    {value: 'bussiness', nombre:'Negoios'},
+    {value: 'sports', nombre:'Deportes'},
+    {value: 'entretainment', nombre:'Entretenimiento'},
+    {value: 'healt', nombre:'Salud'},
+    {value: 'technology', nombre:'Tecnología'},
+  ];
+  paises: any[]=[
+    {value: 'mx', nombre:'Mexico'},
+    {value: 'br', nombre:'Brasil'},
+    {value: 'ar', nombre:'Argentina'},
+    {value: 'fr', nombre:'Francia'},
+    {value: 'hu', nombre:'Hungria'},
+    {value: 'gb', nombre:'Reino Unido'},
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  buscarNoticia(){
+    console.log(this.categoriaSeleccionada);
+    console.log(this.paiseSeleccionado);
+  }
 }
